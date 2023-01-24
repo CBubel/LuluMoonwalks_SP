6c2c43219b8173103f62b55fae2d523533a020f1
CBubel <76258414+CBubel@users.noreply.github.com>
2023-01-23 19:18:44 -0600

    Update settings.py

--- application/settings.py
+++ application/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'senior_project.urls'
+ROOT_URLCONF = 'LuluMoonwalks_SP.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'senior_project.wsgi.application'
+WSGI_APPLICATION = 'LuluMoonwalks_SP.wsgi.application'
 
 
 # Database
