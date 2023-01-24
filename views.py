e1f68c555d684393925e650b472ca262cfffae3a
CBubel <bubelc3087@uhcl.edu>
2023-01-24 13:26:42 -0600

    Merge remote-tracking branch 'origin/master'

--- views.py
+++ views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import login
+from application.authentication import models
 
 
 def admin_login_view(request):
     if request.method == 'POST':
         submitted_username = request.POST['username']
         submitted_password = request.POST['password']
-        admin = authenticate(request, username=submitted_username, password=submitted_password)
+        admin = models.Admin(request, username=submitted_username, password=submitted_password)
+        admin.save()
         if admin is not None:
             login(request, admin)
             return redirect('management.html')
